c417676aa47441d8a308c72cd62c93d12d4956d4
shamim-emon <emon.dev.bd@gmail.com>
2022-11-10 04:52:10 +0600

    Test Suite for unit test created

---
+++ app/src/test/java/bd/emon/movies/TestSuite.kt
@@ -0,0 +1,13 @@
+package bd.emon.movies
+
+import bd.emon.movies.rest.MovieRestRepositoryTest
+import bd.emon.movies.viewModels.HomeViewModelTest
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+    HomeViewModelTest::class,
+    MovieRestRepositoryTest::class
+)
+class TestSuite
